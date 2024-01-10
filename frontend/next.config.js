a1645e94c0879a5a0baf2be7090a5a3c396eee65
Harish Prasanna V <45291139+HarishPrasannaV@users.noreply.github.com>
2024-01-10 12:52:05 +0530

    I am complete

---
+++ frontend/next.config.js
@@ -0,0 +1,12 @@
+const { webpack } = require("next/dist/compiled/webpack/webpack");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ frontend/pages/index.js
@@ -0,0 +1,374 @@
+import {
+  CryptoTardsDAOABI,
+  CryptoTardsDAOAddress,
+  CryptoTardsNFTABI,
+  CryptoTardsNFTAddress,
+} from "@/constants";
+
+import ConnectButton from "@rainbow-me/rainbowkit";
+
+import Head from "next/head";
+
+import { useEffect, useState } from "react";
+
+import { formatEther } from "viem/utils";
+
+import { useAccount, useBalance, useContractRead } from "wagmi";
+
+import { readContract, waitForTransaction, writeContract } from "wagmi/actions";
+
+import styles from "../styles/Home.module.css";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function Home() {
+  // Using wagmi hook to check for wallet
+  const { address, isConnected } = useAccount();
+  // State variable to check if component is mounted
+  const [isMounted, setIsMounted] = useState(false);
+  // State varible to shouw loading state when transaction is processing
+  const [loading, setLoading] = useState(false);
+  // Fake NFT Token ID to purchase. Used when creating a proposal.
+  const [fakeNftTokenId, setFakeNftTokenId] = useState("");
+  // State variable to store all proposals in the DAO
+  const [proposals, setProposals] = useState([]);
+  // State variable to switch between the 'Create Proposal' and 'View Proposals' tabs
+  const [selectedTab, setSelectedTab] = useState("");
+
+  // Fetch the owner of the DAO
+  const daoOwner = useContractRead({
+    abi: CryptoTardsDAOABI,
+    address: CryptoTardsDAOAddress,
+    functionName: "owner",
+  });
+
+  // Getting the blance of DAO
+  const daoBalance = useBalance({
+    address: CryptoTardsDAOAddress,
+  });
+
+  // getting the number of proposals
+  const numOfProposalsInDAO = useContractRead({
+    abi: CryptoTardsDAOABI,
+    address: CryptoTardsDAOAddress,
+    functionName: "numProposals",
+  });
+
+  // getting the nft balance of the user
+  const nftBalanceOfUser = useContractRead({
+    abi: CryptoTardsNFTABI,
+    address: CryptoTardsNFTAddress,
+    functionName: "balanceOf",
+    args: [address],
+  });
+
+  // function to create proposal transaction
+  async function createProposal() {
+    setLoading(true);
+
+    try {
+      const tx = await writeContract({
+        address: CryptoTardsDAOAddress,
+        abi: CryptoTardsDAOABI,
+        functionName: "createProposal",
+        args: [fakeNftTokenId],
+      });
+
+      await waitForTransaction(tx);
+    } catch (error) {
+      console.error(error);
+      window.alert(error);
+    }
+    setLoading(false);
+  }
+
+  // function to fetch a proposal by its id
+  async function fetchProposalById(id) {
+    try {
+      const proposal = await readContract({
+        address: CryptoTardsDAOAddress,
+        abi: CryptoTardsDAOABI,
+        functionName: "proposals",
+        args: [id],
+      });
+
+      const [nftTokenId, deadline, yayVotes, nayVotes, executed] = proposal;
+
+      const parsedProposal = {
+        proposalId: id,
+        nftTokenId: nftTokenId.toString(),
+        deadline: new Date(parseInt(deadline.toString()) * 1000),
+        yayVotes: yayVotes.toString(),
+        nayVotes: nayVotes.toString(),
+        executed: Boolean(executed),
+      };
+
+      return parsedProposal;
+    } catch (error) {
+      console.error(error);
+      window.alert(error);
+    }
+  }
+
+  // function to fetch all proposals
+  async function fetchAllProposals() {
+    try {
+      const proposals = [];
+
+      for (let i = 0; i < numOfProposalsInDAO.data; i++) {
+        const proposal = await fetchProposalById(i);
+        proposals.push(proposal);
+      }
+
+      setProposals(proposals);
+
+      return proposals;
+    } catch (error) {
+      console.error(error);
+      window.alert(error);
+    }
+  }
+
+  // function to vote on proposal
+  async function voteForProposal(proposalId, vote) {
+    setLoading(true);
+    try {
+      const tx = await writeContract({
+        address: CryptoTardsDAOAddress,
+        abi: CryptoTardsDAOABI,
+        functionName: "voteOnProposal",
+        args: [proposalId, vote === "YAY" ? 0 : 1],
+      });
+
+      await waitForTransaction(tx);
+    } catch (error) {
+      console.error(error);
+      window.alert(error);
+    }
+    setLoading(false);
+  }
+
+  //function to execute proposal
+  async function executeProposal(proposalId) {
+    setLoading(true);
+    try {
+      const tx = await writeContract({
+        address: CryptoTardsDAOAddress,
+        abi: CryptoTardsDAOABI,
+        functionName: "executeProposal",
+        args: [proposalId],
+      });
+
+      await waitForTransaction(tx);
+    } catch (error) {
+      console.error(error);
+      window.alert(error);
+    }
+    setLoading(false);
+  }
+
+  //function to withdraw ether from dao
+  async function withdrawDAOEther() {
+    setLoading(true);
+    try {
+      const tx = await writeContract({
+        address: CryptoTardsDAOAddress,
+        abi: CryptoTardsDAOABI,
+        functionName: "withdrawEther",
+      });
+
+      await waitForTransaction(tx);
+    } catch (error) {
+      console.error(error);
+      window.alert(error);
+    }
+    setLoading(false);
+  }
+
+  // rendring content for the selected tab
+
+  function renderTabs() {
+    if (selectedTab === "Create Proposal") {
+      return renderCreateProposalTab();
+    } else if (selectedTab === "View Proposals") {
+      return renderViewProposalsTab();
+    }
+    return null;
+  }
+  // Renders the 'Create Proposal' tab content
+  function renderCreateProposalTab() {
+    if (loading) {
+      return (
+        <div className={styles.description}>
+          Loading... Waiting for transaction...
+        </div>
+      );
+    } else if (nftBalanceOfUser.data === 0) {
+      return (
+        <div className={styles.description}>
+          You do not own any CryptoTards NFTs. <br />
+          <b>You cannot create or vote on proposals</b>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.container}>
+          <label>Fake NFT Token ID to Purchase: </label>
+          <input
+            placeholder="0"
+            type="number"
+            onChange={(e) => setFakeNftTokenId(e.target.value)}
+          />
+          <button className={styles.button2} onClick={createProposal}>
+            Create
+          </button>
+        </div>
+      );
+    }
+  }
+
+  // Renders the 'View Proposals' tab content
+  function renderViewProposalsTab() {
+    if (loading) {
+      return (
+        <div className={styles.description}>
+          Loading... Waiting for transaction...
+        </div>
+      );
+    } else if (proposals.length === 0) {
+      return (
+        <div className={styles.description}>No proposals have been created</div>
+      );
+    } else {
+      return (
+        <div>
+          {proposals.map((p, index) => (
+            <div key={index} className={styles.card}>
+              <p>Proposal ID: {p.proposalId}</p>
+              <p>Fake NFT to Purchase: {p.nftTokenId}</p>
+              <p>Deadline: {p.deadline.toLocaleString()}</p>
+              <p>Yay Votes: {p.yayVotes}</p>
+              <p>Nay Votes: {p.nayVotes}</p>
+              <p>Executed: {p.executed.toString()}</p>
+              {p.deadline.getTime() > Date.now() && !p.executed ? (
+                <div className={styles.flex}>
+                  <button
+                    className={styles.button2}
+                    onClick={() => voteForProposal(p.proposalId, "YAY")}
+                  >
+                    Vote YAY
+                  </button>
+                  <button
+                    className={styles.button2}
+                    onClick={() => voteForProposal(p.proposalId, "NAY")}
+                  >
+                    Vote NAY
+                  </button>
+                </div>
+              ) : p.deadline.getTime() < Date.now() && !p.executed ? (
+                <div className={styles.flex}>
+                  <button
+                    className={styles.button2}
+                    onClick={() => executeProposal(p.proposalId)}
+                  >
+                    Execute Proposal{" "}
+                    {p.yayVotes > p.nayVotes ? "(YAY)" : "(NAY)"}
+                  </button>
+                </div>
+              ) : (
+                <div className={styles.description}>Proposal Executed</div>
+              )}
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
+  // Piece of code that runs everytime the value of `selectedTab` changes
+  // Used to re-fetch all proposals in the DAO when user switches
+  // to the 'View Proposals' tab
+  useEffect(() => {
+    if (selectedTab === "View Proposals") {
+      fetchAllProposals();
+    }
+  }, [selectedTab]);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  if (!isConnected)
+    return (
+      <div>
+        <ConnectButton />
+      </div>
+    );
+
+  return (
+    <div className={inter.className}>
+      <Head>
+        <title>CryptoTards DAO</title>
+        <meta name="description" content="CryptoTards DAO" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Tards!</h1>
+          <div className={styles.description}>Welcome to the DAO!</div>
+          <div className={styles.description}>
+            Your CryptoTards NFT Balance: {nftBalanceOfUser.data.toString()}
+            <br />
+            {daoBalance.data && (
+              <>
+                Treasury Balance:{" "}
+                {formatEther(daoBalance.data.value).toString()} ETH
+              </>
+            )}
+            <br />
+            Total Number of Proposals: {numOfProposalsInDAO.data.toString()}
+          </div>
+          <div className={styles.flex}>
+            <button
+              className={styles.button}
+              onClick={() => setSelectedTab("Create Proposal")}
+            >
+              Create Proposal
+            </button>
+            <button
+              className={styles.button}
+              onClick={() => setSelectedTab("View Proposals")}
+            >
+              View Proposals
+            </button>
+          </div>
+          {renderTabs()}
+          {/* Display additional withdraw button if connected wallet is owner */}
+          {address && address.toLowerCase() === daoOwner.data.toLowerCase() ? (
+            <div>
+              {loading ? (
+                <button className={styles.button}>Loading...</button>
+              ) : (
+                <button className={styles.button} onClick={withdrawDAOEther}>
+                  Withdraw DAO ETH
+                </button>
+              )}
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div>
+          <img className={styles.image} src="https://i.imgur.com/buNhbF7.png" />
+        </div>
+      </div>
+    </div>
+  );
+}
